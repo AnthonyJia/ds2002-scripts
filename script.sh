a494695dfc86882ff759b3be65b2b49359ccc323
AnthonyJia <anthonyjiang117@gmail.com>
2025-02-03 16:38:34 -0500

    scripting practice

---
+++ script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "The first positional variable is: $1"
+echo "The second positional variale is: $2"
+echo "The third positional variable is: $3"
+echo "The zeroeth positional variable is $0"
+date
